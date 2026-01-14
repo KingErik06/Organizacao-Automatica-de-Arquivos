2a366981dab52d1ec5ea6fef8e39004aa43e9a88
Erik <eerik2729@gmail.com>
2026-01-13 23:03:11 -0300

    feat: list files from input directory

--- organizacao_automatica_de_arquivos/src/main.py
+++ organizacao_automatica_de_arquivos/src/main.py
@@ -1,8 +1,14 @@
-def main():
-    print("Automação iniciada...")
-    print("Processando dados...")
-    print("Automação finalizada com sucesso!")
+from pathlib import Path
+import shutil
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+INPUT_DIR = BASE_DIR / "data" / "input"
+
+def organizar_arquivos():
+    for item in INPUT_DIR.iterdir():
+        if item.is_file():
+            print(item.name)
 
 
 if __name__ == "__main__":
-    main()
+    organizar_arquivos()
