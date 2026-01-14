38aba4a54f64bbf7682352eeee90d655f3d4c912
Erik <eerik2729@gmail.com>
2026-01-13 23:29:00 -0300

    feat: move files in exstension folders

--- organizacao_automatica_de_arquivos/src/main.py
+++ organizacao_automatica_de_arquivos/src/main.py
@@ -6,9 +6,24 @@ INPUT_DIR = BASE_DIR / "data" / "input"
 
 def organizar_arquivos():
     for item in INPUT_DIR.iterdir():
-        if item.is_file():
-            print(item.name)
+        if not item.is_file():
+            continue
 
+        extensao = item.suffix.lower()
+
+        if not extensao:
+            continue
+
+        extensao = extensao.replace(".", "")
+        pasta_destino = INPUT_DIR / extensao
+
+        if not pasta_destino.exists():
+            pasta_destino.mkdir()
+
+        destino = pasta_destino / item.name
+        print(f"Movendo {item.name} -> {pasta_destino.name}/")
+
+        item.rename(destino)
 
 if __name__ == "__main__":
     organizar_arquivos()
